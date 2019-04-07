9656fa48381f3539662de5fd896a94ee94210835
Steven <spikegreen2@gmail.com>
2019-04-07 09:07:32 +0800

    Initial Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Random Quote Machine</h1>
+      </div>
+    )
+  }
+};
