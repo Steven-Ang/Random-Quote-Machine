c701d5fb34c961399fe72ee70dbd7bc4972a8458
Steven <spikegreen2@gmail.com>
2019-04-10 21:45:11 +0800

    Updates

---
+++ precache-manifest.e65fd0e14691a4b4aca9c0da147c17cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fef1ef9d8dbac07cad7",
+    "url": "/Random-Quote-Machine/static/css/main.8ae77de3.chunk.css"
+  },
+  {
+    "revision": "5fef1ef9d8dbac07cad7",
+    "url": "/Random-Quote-Machine/static/js/main.229f77d3.chunk.js"
+  },
+  {
+    "revision": "fc1abb5e9f8eb11e4c02",
+    "url": "/Random-Quote-Machine/static/js/runtime~main.6010cb3b.js"
+  },
+  {
+    "revision": "d595b206f3f49dbbd917",
+    "url": "/Random-Quote-Machine/static/js/2.4157e642.chunk.js"
+  },
+  {
+    "revision": "4f08367b4985f3c5f21eb5d597e27829",
+    "url": "/Random-Quote-Machine/index.html"
+  }
+];
